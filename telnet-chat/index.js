11251b984b28b1185b76856713c14403f408e664
lihw <MajyoArisu@outlook.com>
2018-05-20 22:45:25 +0800

    add telnet-chat

---
+++ telnet-chat/index.js
@@ -0,0 +1,40 @@
+var net = require('net');
+var count = 0;
+var users = {};
+
+var server = net.createServer(function (conn) {
+  var nickname;
+  count++;
+  conn.setEncoding('utf8');
+  conn.write(
+    '\n > welcome to \033[92mnode-chat\033[39m!'
+    + '\n > ' + count + ' other people are connected at this time.'
+    + '\n > please write your name and press Enter: '
+  );
+  conn.on('close', function () {
+    count--;
+  });
+  conn.on('data', function (data) {
+    data = data.replace('\r\n', '');
+    if (!nickname) {
+      if (users[data]) {
+        conn.write('\033[93mnickname already in use. try again: \033[39m');
+        return;
+      } else {
+        nickname = data;
+        users[nickname] = conn;
+        for (var i in users) {
+          users[i].write('\033[90m> ' + nickname + ' joined the room\033[39m\n');
+        }
+      }
+    } else {
+      for (var i in users) {
+        users[i].write('\033[96m> ' + nickname + ': \033[39m' + data + '\n');
+      }
+    }
+  });
+});
+
+server.listen(3000, function () {
+  console.log('\033[96mserver listening on *:3000\033[39m');
+});
